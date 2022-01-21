45d9ec60a58009ea343b6fdece552d9470cfafb3
isaacmg00 <43230315+isaacmg00@users.noreply.github.com>
2022-01-20 23:40:27 -0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,30 @@
+
+window.addEventListener('load', function () {
+
+	var game = new Phaser.Game({
+		width: 800,
+		height: 600,
+		type: Phaser.AUTO,
+        backgroundColor: "#242424",
+		scale: {
+			mode: Phaser.Scale.FIT,
+			autoCenter: Phaser.Scale.CENTER_BOTH
+		}
+	});
+	
+	game.scene.add("Preload", Preload);
+	game.scene.add("MainMenu", MainMenu);
+	game.scene.add("Beach", Beach);
+
+	game.scene.add("Boot", Boot, true);
+});
+
+class Boot extends Phaser.Scene {
+
+	preload() {
+		
+		this.load.pack("pack", "assets/preload-asset-pack.json");
+
+		this.load.on(Phaser.Loader.Events.COMPLETE, () => this.scene.start("Preload"));
+	}
+}
