39cb38b0b66ff6732c706d613120ae8369a9de41
isaacmg00 <43230315+isaacmg00@users.noreply.github.com>
2022-01-21 23:51:28 -0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,28 +1,33 @@
+window.addEventListener("load", function () {
+  var game = new Phaser.Game({
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO,
+    backgroundColor: "#242424",
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+  });
 
-window.addEventListener('load', function () {
+  game.scene.add("Preload", Preload);
+  game.scene.add("MainMenu", MainMenu);
+  game.scene.add("Beach", Beach);
+  game.scene.add("Start", Start);
+  game.scene.add("Tutorial", Tutorial);
+  game.scene.add("Controls", Controls);
+  game.scene.add("Settings", Settings);
+  game.scene.add("Credits", Credits);
 
-	var game = new Phaser.Game({
-		width: 800,
-		height: 600,
-		type: Phaser.AUTO,
-        backgroundColor: "#242424",
-		scale: {
-			mode: Phaser.Scale.FIT,
-			autoCenter: Phaser.Scale.CENTER_BOTH
-		}
-	});
-	
-	game.scene.add("Preload", Preload);
-	game.scene.add("MainMenu", MainMenu);
-	game.scene.add("Boot", Boot, true);
+  game.scene.add("Boot", Boot, true);
 });
 
 class Boot extends Phaser.Scene {
+  preload() {
+    this.load.pack("pack", "assets/preload-asset-pack.json");
 
-	preload() {
-		
-		this.load.pack("pack", "assets/preload-asset-pack.json");
-
-		this.load.on(Phaser.Loader.Events.COMPLETE, () => this.scene.start("Preload"));
-	}
+    this.load.on(Phaser.Loader.Events.COMPLETE, () =>
+      this.scene.start("Preload")
+    );
+  }
 }
