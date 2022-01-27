0c2e360d5b9d6bdebf9f7a5e00295cac7cf96403
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-26 19:58:26 -0800

    replaced styling of credits scene

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -16,8 +16,54 @@ class Credits extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
-		// credits_banner_title
-		this.add.image(840, 426, "credits-banner-title");
+		// main-menu-background
+		const main_menu_background = this.add.container(418, 314);
+
+		// layer_0
+		const layer_0 = this.add.image(-6, -4, "main-menu-environment-sky");
+		layer_0.scaleX = 0.19;
+		layer_0.scaleY = 0.28;
+		main_menu_background.add(layer_0);
+
+		// layer_1
+		const layer_1 = this.add.image(-6, -98, "main-menu-environment-cloud");
+		layer_1.scaleX = 0.2;
+		layer_1.scaleY = 0.2;
+		main_menu_background.add(layer_1);
+
+		// layer_2
+		const layer_2 = this.add.image(-3, 122, "main-menu-environment-tree1");
+		layer_2.scaleX = 0.19;
+		layer_2.scaleY = 0.17;
+		main_menu_background.add(layer_2);
+
+		// layer_3
+		const layer_3 = this.add.image(-9, 107, "main-menu-environment-tree2");
+		layer_3.scaleX = 0.19;
+		layer_3.scaleY = 0.17;
+		main_menu_background.add(layer_3);
+
+		// layer_4
+		const layer_4 = this.add.image(-4, 112, "main-menu-environment-tree");
+		layer_4.scaleX = 0.19;
+		layer_4.scaleY = 0.17;
+		main_menu_background.add(layer_4);
+
+		// button_back
+		const button_back = this.add.image(88, 87, "button-back");
+		button_back.scaleX = 0.15;
+		button_back.scaleY = 0.15;
+
+		button_back.setInteractive();
+
+		button_back.on("pointerdown", () => {
+		this.scene.start("MainMenu");
+		});
+
+		// credits_textbox_content5
+		const credits_textbox_content5 = this.add.image(420, 301, "credits-textbox-content5");
+		credits_textbox_content5.scaleX = 0.3;
+		credits_textbox_content5.scaleY = 0.3;
 
 		this.events.emit("scene-awake");
 	}
