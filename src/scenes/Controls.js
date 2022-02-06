25ca2797bceb6d42db3c6742df667dcde1491d3f
itzraytothedon <92879365+itzraytothedon@users.noreply.github.com>
2022-02-05 20:29:34 -0800

    Cursors + Back function added
    Added in a png for the cursor and also made the back button functional on tutorial, settings, and controls.

--- src/scenes/Controls.js
+++ src/scenes/Controls.js
@@ -3,67 +3,93 @@
 /* START OF COMPILED CODE */
 
 class Controls extends Phaser.Scene {
-  constructor() {
-    super("Controls");
 
-    /* START-USER-CTR-CODE */
+	constructor() {
+		super("Controls");
+
+		/* START-USER-CTR-CODE */
     // Write your code here.
     /* END-USER-CTR-CODE */
-  }
+	}
 
-  /** @returns {void} */
-  editorCreate() {
-    // main-menu-background
-    const main_menu_background = this.add.container(407, 309);
+	/** @returns {void} */
+	editorCreate() {
 
-    // layer_0
-    const layer_0 = this.add.image(-6, -4, "main-menu-environment-sky");
-    layer_0.scaleX = 0.19;
-    layer_0.scaleY = 0.28;
-    main_menu_background.add(layer_0);
+		// main-menu-background
+		const main_menu_background = this.add.container(407, 309);
 
-    // layer_1
-    const layer_1 = this.add.image(-6, -98, "main-menu-environment-cloud");
-    layer_1.scaleX = 0.2;
-    layer_1.scaleY = 0.2;
-    main_menu_background.add(layer_1);
+		// layer_0
+		const layer_0 = this.add.tileSprite(-6, -4, 4320, 2160, "main-menu-environment-sky");
+		layer_0.scaleX = 0.19;
+		layer_0.scaleY = 0.28;
+		main_menu_background.add(layer_0);
 
-    // layer_2
-    const layer_2 = this.add.image(-3, 122, "main-menu-environment-tree1");
-    layer_2.scaleX = 0.19;
-    layer_2.scaleY = 0.17;
-    main_menu_background.add(layer_2);
+		// layer_1
+		const layer_1 = this.add.tileSprite(-6, -98, 4320, 2160, "main-menu-environment-cloud");
+		layer_1.scaleX = 0.2;
+		layer_1.scaleY = 0.2;
+		main_menu_background.add(layer_1);
 
-    // layer_3
-    const layer_3 = this.add.image(-9, 107, "main-menu-environment-tree2");
-    layer_3.scaleX = 0.19;
-    layer_3.scaleY = 0.17;
-    main_menu_background.add(layer_3);
+		// layer_2
+		const layer_2 = this.add.tileSprite(5, 122, 4320, 2160, "main-menu-environment-tree1");
+		layer_2.scaleX = 0.2;
+		layer_2.scaleY = 0.17;
+		main_menu_background.add(layer_2);
 
-    this.events.emit("scene-awake");
+		// layer_3
+		const layer_3 = this.add.tileSprite(0, 107, 4320, 2160, "main-menu-environment-tree2");
+		layer_3.scaleX = 0.19;
+		layer_3.scaleY = 0.17;
+		main_menu_background.add(layer_3);
 
-    const helloButton = this.add.text(
-      100,
-      100,
-      "controls scene --- click to return to main menu",
-      {
-        fill: "#000000",
-      }
-    );
-    helloButton.setInteractive();
+		// layer_4
+		const layer_4 = this.add.tileSprite(-4, 112, 4320, 2160, "main-menu-environment-tree");
+		layer_4.scaleX = 0.19;
+		layer_4.scaleY = 0.17;
+		main_menu_background.add(layer_4);
 
-    helloButton.on("pointerdown", () => {
-      this.scene.start("MainMenu");
-    });
-  }
+		// button_back
+		const button_back = this.add.image(88, 87, "button-back");
+		button_back.scaleX = 0.15;
+		button_back.scaleY = 0.15;
+		button_back.setInteractive();
+		button_back.on("pointerdown", () => {
+		this.scene.start("MainMenu");
+		});
 
-  /* START-USER-CODE */
+		this.layer_0 = layer_0;
+		this.layer_1 = layer_1;
+		this.layer_2 = layer_2;
+		this.layer_3 = layer_3;
+		this.layer_4 = layer_4;
+
+		this.events.emit("scene-awake");
+	}
+
+	/** @type {Phaser.GameObjects.TileSprite} */
+	layer_0;
+	/** @type {Phaser.GameObjects.TileSprite} */
+	layer_1;
+	/** @type {Phaser.GameObjects.TileSprite} */
+	layer_2;
+	/** @type {Phaser.GameObjects.TileSprite} */
+	layer_3;
+	/** @type {Phaser.GameObjects.TileSprite} */
+	layer_4;
+
+	/* START-USER-CODE */
 
   // Write more your code here
 
   create() {
     this.editorCreate();
   }
+  update(){
+		this.layer_1.tilePositionX += 1;
+		this.layer_2.tilePositionX += 0.4;
+		this.layer_3.tilePositionX += 0.6;
+		this.layer_4.tilePositionX += 4.5;
+  	}
 
   /* END-USER-CODE */
 }
