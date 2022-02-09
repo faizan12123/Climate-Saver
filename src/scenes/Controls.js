364aa09faebad0b221cd4f338e887742804f8df0
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-09 11:01:49 -0800

    added credits GUI + radio buttons to scene

--- src/scenes/Controls.js
+++ src/scenes/Controls.js
@@ -5,7 +5,7 @@
 class Controls extends Phaser.Scene {
 
 	constructor() {
-		super("Controls");
+		super("Credits");
 
 		/* START-USER-CTR-CODE */
     // Write your code here.
@@ -14,10 +14,6 @@ class Controls extends Phaser.Scene {
 
 	/** @returns {void} */
 	editorCreate() {
-		//background music
-		var backgroundMusic = this.sound.add("main-menu");
-		backgroundMusic.play();
-		backgroundMusic.loop = true;
 
 		// main-menu-background
 		const main_menu_background = this.add.container(407, 309);
@@ -52,24 +48,35 @@ class Controls extends Phaser.Scene {
 		layer_4.scaleY = 0.17;
 		main_menu_background.add(layer_4);
 
-		// Button Sounds
-    	var buttonClicked = this.sound.add("buttonOnClick");
-
 		// button_back
 		const button_back = this.add.image(88, 87, "button-back");
 		button_back.scaleX = 0.15;
 		button_back.scaleY = 0.15;
-		button_back.setInteractive();
-		button_back.on("pointerdown", () => {
-			backgroundMusic.stop();
-			buttonClicked.play();
-			this.scene.start("MainMenu");
-		}).on("pointerover", () => {
-      		button_back.scale += 0.05;
-    	}).on("pointerout", () => {
-			button_back.scaleX = 0.15;
-			button_back.scaleY = 0.15;
-		});
+
+		// controlsOF
+		const controlsOF = this.add.image(407, 309, "controlsOF");
+		controlsOF.scaleX = 0.35;
+		controlsOF.scaleY = 0.35;
+
+		// button_radio
+		const button_radio = this.add.image(237, 283, "button-radio");
+		button_radio.scaleX = 0.5;
+		button_radio.scaleY = 0.5;
+
+		// button_radio_1
+		const button_radio_1 = this.add.image(240, 377, "button-radio");
+		button_radio_1.scaleX = 0.5;
+		button_radio_1.scaleY = 0.5;
+
+		// button_tick
+		const button_tick = this.add.image(236, 284, "button-tick");
+		button_tick.scaleX = 0.1;
+		button_tick.scaleY = 0.1;
+
+		// button_tick_1
+		const button_tick_1 = this.add.image(239, 376, "button-tick");
+		button_tick_1.scaleX = 0.1;
+		button_tick_1.scaleY = 0.1;
 
 		this.layer_0 = layer_0;
 		this.layer_1 = layer_1;
