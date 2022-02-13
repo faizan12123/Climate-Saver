0a7684420c1feea4faa7dc91ad6cae9d5145e1d9
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-12 22:46:52 -0800

    added layers

--- src/scenes/City.js
+++ src/scenes/City.js
@@ -15,7 +15,13 @@ class City extends Phaser.Scene {
 
 	/** @returns {void} */
 	editorCreate() {
+        const map = this.make.tilemap({key: "cityV1"});
+        const tileset = map.addTilesetImage("city-tilesV1", "city-tiles")
 
+        const belowLayer = map.createLayer("Ground", tileset, 0,0);
+        const worldLayer = map.createLayer("World Layer", tileset, 0,0);
+        const worldLayer2 = map.createLayer("Windows and Doors", tileset, 0,0);
+        const worldObjects = map.createLayer("Lights", tileset, 0,0);
 	}
 
 	/* START-USER-CODE */
