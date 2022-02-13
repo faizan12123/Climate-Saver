0662e019886d5fafeda710d805fc33fbddf876ec
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-12 22:33:46 -0800

    new city script and scene

---
+++ src/scenes/City.js
@@ -0,0 +1,35 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+class City extends Phaser.Scene {
+
+	constructor() {
+		super("City");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @returns {void} */
+	editorCreate() {
+
+	}
+
+	/* START-USER-CODE */
+
+	// Write more your code here
+
+	create() {
+
+		this.editorCreate();
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
