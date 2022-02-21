fbe0dd5f01821b578596a24350195aba43c589ec
Faizan <faizan121234@gmail.com>
2022-02-20 18:28:26 -0800

    changing the character.png to dude.png

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -182,6 +182,83 @@ class MainMenu extends Phaser.Scene {
     });
 
 
+
+    //******************character movement********************
+    var player;
+    var cursors;
+
+
+    function preload ()
+    {
+        this.load.spritesheet('dude', 'assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    function create ()
+    {
+
+
+        player = this.physics.add.sprite(100, 450, 'dude');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    function update ()
+    {
+        if (cursors.left.isDown)
+        {
+            player.setVelocityX(-160);
+
+            player.anims.play('left', true);
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(160);
+
+            player.anims.play('right', true);
+        }
+        else
+        {
+            player.setVelocityX(0);
+
+            player.anims.play('turn');
+        }
+
+        if (cursors.up.isDown && player.body.touching.down)
+        {
+            player.setVelocityY(-330);
+        }
+    }
+
+
+
+
+
+
+
+
   /** @type {Phaser.GameObjects.TileSprite} */
   this.layer_0 =layer_0;
   /** @type {Phaser.GameObjects.TileSprite} */
@@ -201,13 +278,16 @@ class MainMenu extends Phaser.Scene {
 
   create() {
     this.editorCreate();
+    
   }
+
+
   update() {
     this.layer_1.tilePositionX += 1;
     this.layer_2.tilePositionX += 0.4;
     this.layer_3.tilePositionX += 0.6;
     this.layer_4.tilePositionX += 4.5;
-    
+
   }
 
   /* END-USER-CODE */
