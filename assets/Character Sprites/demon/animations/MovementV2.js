0c91e00d5009464e7e70831a63fd6aaf02e1fa9e
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-28 22:02:53 -0700

    new class objects physics and movmenet

--- assets/Character Sprites/demon/animations/MovementV2.js
+++ assets/Character Sprites/demon/animations/MovementV2.js
@@ -8,10 +8,12 @@ class MovementV2 {
 	constructor(gameObject) {
 		this.gameObject = gameObject;
 		gameObject["__MovementV2"] = this;
-		const scene = this.gameObject.scene;
 
-		this.cursors = scene.input.keyboard.createCursorKeys();
 		/* START-USER-CTR-CODE */
+
+		const scene = this.gameObject.scene;
+		this.cursors = scene.input.keyboard.createCursorKeys();
+		scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
 		// Write your code here.
 		/* END-USER-CTR-CODE */
 	}
@@ -25,12 +27,53 @@ class MovementV2 {
 	gameObject;
 
 	/* START-USER-CODE */
+	update(){
+		const speed = 200
+		const player = this.gameObject
+		const body  = player.body;
+		if(!body){
+			return;
+		}
 
-	// Write your code here.
-
-	/* END-USER-CODE */
+		if (this.cursors.left.isDown)
+		{
+			body.setVelocity(-speed, 0)
+			player.play('up-walk', true)
+		}
+		else if (this.cursors.right.isDown)
+		{
+			body.setVelocity(speed, 0)
+			player.play('down-walk', true)
+		}
+		else if (this.cursors.up.isDown)
+		{
+			body.setVelocity(0, -speed)
+			player.play('up-walk', true)
+		}
+		else if (this.cursors.down.isDown)
+		{
+			body.setVelocity(0, speed)
+			player.play('down-walk', true)
+		}
+		else if(this.cursors.space.isDown){
+			body.setVelocity(0, 0)
+			const key = player.anims.currentAnim.key
+			const parts = key.split('-')
+			const direction = parts[0]
+			console.log("-"+direction+"-")
+			player.play(direction+"-attack", true);
+		}
+		else
+		{
+			body.setVelocity(0, 0)
+			const key = player.anims.currentAnim.key
+			const parts = key.split('-')
+			const direction = parts[0]
+			console.log("-"+direction+"-")
+			player.play(direction+"-idle", true);
+		}
+	}
 }
 
-/* END OF COMPILED CODE */
 
 // You can write more code here
