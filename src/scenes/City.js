e46f77c3a38fc05cdcf6e550586a56ad6b255340
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-04-05 23:47:55 -0700

    buildings can no longer be walked on by character

--- src/scenes/City.js
+++ src/scenes/City.js
@@ -7,42 +7,14 @@ class City extends Phaser.Scene {
 
 	constructor() {
 		super("City");
-
-		/* START-USER-CTR-CODE */
-		// Write your code here.
-		/* END-USER-CTR-CODE */
 	}
 
 	/** @returns {void} */
 	editorCreate() {
+		this.displayMap();
+		this.displayPlayer();
 
-
-		// cityV2
-		const cityV2 = this.add.tilemap("cityV2");
-		cityV2.addTilesetImage("City", "city-tilesV1");
-
-
-		// ground_1
-		const ground_1 = cityV2.createLayer("Ground", ["City"], 4, -97);
-		ground_1.scaleX = 1.25;
-		ground_1.scaleY = 1.25;
-
-		// world_Layer
-		const world_Layer = cityV2.createLayer("World Layer", ["City"], 4, -97);
-		world_Layer.scaleX = 1.25;
-		world_Layer.scaleY = 1.25;
-
-		// lights
-		const lights = cityV2.createLayer("Lights", ["City"], 4, -97);
-		lights.scaleX = 1.25;
-		lights.scaleY = 1.25;
-
-		// windows_and_Doors
-		const windows_and_Doors = cityV2.createLayer("Windows and Doors", ["City"], 4, -97);
-		windows_and_Doors.scaleX = 1.25;
-		windows_and_Doors.scaleY = 1.25;
-
-		this.cityV2 = cityV2;
+		
 
 		function formatTime(seconds){
     		// Minutes
@@ -225,15 +197,8 @@ class City extends Phaser.Scene {
 		fx_tick.visible = false;
 
 
-		this.cityV2 = cityV2;
-
 		/*--------------GAMEPLAY THINGS--------------- */
 		this.add.text(20, 0, 'Space Bar to hit things. Arrows to move', { fontFamily: "Georgia", fontSize: "40px", color: "yellow" });
-		// player
-		const player = this.add.sprite(489, 348, "1_1");
-		new PhysicsV2(player);
-		new MovementV2(player);
-		this.player = player;
 
 		this.events.emit("scene-awake");
 	}
@@ -246,12 +211,44 @@ class City extends Phaser.Scene {
 
 		this.editorCreate();
 		this.player.play("down-idle");
+		this.physics.add.collider(this.player, this.worldLayer)
+	}
+	update(){
+		this.worldLayer.setCollisionByProperty({ collides: true });
 	}
 
-	/* END-USER-CODE */
+	displayMap(){
+		const cityV2 = this.add.tilemap("cityV2");
+		cityV2.addTilesetImage("City", "city-tilesV1");
+
+		const ground_1 = cityV2.createLayer("Ground", ["City"], 4, -97);
+		ground_1.scaleX = 1.25;
+		ground_1.scaleY = 1.25;
+
+		// world_Layer
+		this.worldLayer = cityV2.createLayer("World Layer", ["City"], 4, -97);
+		this.worldLayer.scaleX = 1.25;
+		this.worldLayer.scaleY = 1.25;
+
+		// lights
+		const lights = cityV2.createLayer("Lights", ["City"], 4, -97);
+		lights.scaleX = 1.25;
+		lights.scaleY = 1.25;
+
+		// windows_and_Doors
+		const windows_and_Doors = cityV2.createLayer("Windows and Doors", ["City"], 4, -97);
+		windows_and_Doors.scaleX = 1.25;
+		windows_and_Doors.scaleY = 1.25;
+
+		this.cityV2 = cityV2;
+	}
+	displayPlayer(){
+		// player
+		const player = this.add.sprite(489, 348, "1_1");
+		new PhysicsV2(player);
+		new MovementV2(player);
+		this.player = player;
+	}
 }
 
-/* END OF COMPILED CODE */
-
-// You can write more code here
 
