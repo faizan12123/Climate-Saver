f9cc948d118ceeeaa530dc37d6fc2337a92435dc
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-04-18 00:20:08 -0700

    minor bug fix

--- assets/Character Sprites/simpleCharacter/animations/MovementV3.js
+++ assets/Character Sprites/simpleCharacter/animations/MovementV3.js
@@ -1,8 +1,3 @@
-
-// You can write more code here
-
-/* START OF COMPILED CODE */
-
 class MovementV3 {
 
 	constructor(gameObject) {
@@ -79,11 +74,3 @@ class MovementV3 {
 	}
 }
 
-
-// You can write more code here
-/* END-USER-CODE */
-}
-
-/* END OF COMPILED CODE */
-
-// You can write more code here
